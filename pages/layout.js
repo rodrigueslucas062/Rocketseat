8371c4d5ab7e36867713d128b37cb9472909a8c4
lucas rodrigues <rodrigueslucas062@gmail.com>
2024-02-08 22:47:16 -0300

    🎨 Global scss and Root Layout

---
+++ pages/layout.js
@@ -0,0 +1,14 @@
+const Layout = ({ children }) => {
+    return (
+        <html lang="pt">
+            <head>
+                <title>Lucas Rodrigues</title>
+            </head>
+            <body>
+                {children}
+            </body>
+        </html>
+    );
+};
+
+export default Layout;
