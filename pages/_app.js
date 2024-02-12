1f429513650525fdd5e2b079b7cd194c2d7614d7
lucas rodrigues <rodrigueslucas062@gmail.com>
2024-02-12 18:45:41 -0300

    ✨ Sonne component

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,11 @@
 import "@/styles/globals.scss";
+import { Toaster } from "sonner";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      <Toaster richColors />
+    </>
+  );
 }
