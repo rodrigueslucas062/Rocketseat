6cddd8dc97a11697d010ae8de4e8eadfe75c570e
lucas rodrigues <rodrigueslucas062@gmail.com>
2024-02-12 20:27:07 -0300

    ✨ Filter, LocalStorage, Create notes

--- pages/components/main/Main.jsx
+++ pages/components/main/Main.jsx
@@ -2,8 +2,40 @@ import Image from "next/image";
 import Logo from "../../../public/images/Logo.png";
 import { NoteCards } from "../notes-cards/NoteCards";
 import NewNoteCards from "../notes-cards/NewNoteCard";
+import { useEffect, useState } from "react";
 
 const Main = () => {
+    const [search, setSearch] = useState('')
+    const [notes, setNotes] = useState([])
+
+    useEffect(() => {
+        const notesOnStorage = localStorage.getItem('notes')
+        if (notesOnStorage) {
+            setNotes(JSON.parse(notesOnStorage))
+        }
+    }, [])
+
+    function onNoteCreated(string) {
+        const newNote = {
+            id: crypto.randomUUID(),
+            date: new Date(),
+            content: string,
+        }
+        const notesArray = [newNote, ...notes]
+        setNotes(notesArray)
+        localStorage.setItem('notes', JSON.stringify(notesArray))
+    }
+
+    function handleSearch(event) {
+        const query = event.target.value
+
+        setSearch(query)
+    }
+
+    const filteredNotes = search !== ''
+        ? notes.filter(note => note.content.toLocaleLowerCase().includes(search.toLocaleLowerCase()))
+        : notes
+
     return (
         <div className="mx-auto max-w-6xl my-12 space-y-6">
             <Image src={Logo} width={100} height={100} />
@@ -13,12 +45,16 @@ const Main = () => {
                     type="text"
                     placeholder="Busque em suas notas..."
                     className="w-full bg-transparent font-semibold tracking-tighter outline-none placeholder:text-slate-500"
+                    onChange={handleSearch}
                 />
             </form>
             <div className="h-px bg-slate-700"></div>
             <div className="grid grid-cols-3 gap-6 auto-rows-[250px]">
-                <NewNoteCards />
-                <NoteCards date={new Date()} content="hello world" />
+                <NewNoteCards onNoteCreated={onNoteCreated} />
+
+                {filteredNotes.map(note => {
+                    return <NoteCards key={note.id} date={note.date} content={note.content} />
+                })}
             </div>
         </div>
     );
